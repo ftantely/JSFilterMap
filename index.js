2020b10d946f31a3ac75df2ae4d8537f7ef90b0c
Tantely Rambeloson <ftantely@gmail.com>
2018-07-01 15:49:55 -0600

    07012018

--- index.js
+++ index.js
@@ -21,7 +21,64 @@ const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 //
 // forEach
 
-companies.forEach(function (company) {
-    console.log(company)
+// companies.forEach(function (company) {
+//     console.log(company)
+//
+// });
+//
+//
+// filter
+//
+// let canDrink = [];
+// for (let i=0; i<ages.length; i++) {
+//     if(ages[i]>=21){
+//         canDrink.push(ages[i]);
+//     }
+// };
+//
+// const canDrink= ages.filter(function(age){
+//     if(age >=21) {
+//         return true;;
+//     }
+// });
 
-});
+// const canDrink=ages.filter(age => age >=21);
+//
+//
+// Get retail
+
+// let retails = companies.filter(company=> company.category==="Retail");
+
+// let retails = companies.filter(function (one) {
+//     if( one.start >=1990 && one.category ==="Retail"){
+//         return true;
+//
+//     }
+//
+// })
+//
+// ES6
+
+
+// const retails = companies.filter(one => one.start>=1990 && one.category ==="Auto")
+// //
+// // console.log(retails);
+
+
+// Map method
+
+//
+// const mapTest = companies.map(function (name) {
+//     return ` ${name.name} [${name.start} - ${name.end}]`;
+//
+// });
+//
+// console.log(mapTest);
+
+// const mapSqrt = ages.map(age=>Math.sqrt(age));
+// console.log(mapSqrt);
+
+const mapSqrtTimesTwo =  ages
+.map(age=>Math.sqrt(age))
+.map(age=>age*2);
+console.log(mapSqrtTimesTwo);
